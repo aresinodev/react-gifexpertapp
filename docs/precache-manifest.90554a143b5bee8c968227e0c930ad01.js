3468278f73603ff72634345ce72dcfa5fc256dcf
Aarón RJ (Developer) <aresinodeveloper@gmail.com>
2020-06-24 12:02:04 +0200

    docs folder added

---
+++ docs/precache-manifest.90554a143b5bee8c968227e0c930ad01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df05a8f7627ce87a6b7b90634131ce30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3480997f62f8e5c3e11b",
+    "url": "/static/css/main.3ff0f9c5.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3480997f62f8e5c3e11b",
+    "url": "/static/js/main.84536d9e.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
