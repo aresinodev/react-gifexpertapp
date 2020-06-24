3e56883f47d73b5d8496eea86f9466119e0da34e
Aarón RJ (Developer) <aresinodeveloper@gmail.com>
2020-06-24 11:50:08 +0200

    Docs added

---
+++ docs/precache-manifest.27a2b2e0ac432f7e1fb3d3c83de3a06b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ea5df0c88c4a3ed49a23d8df924242",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4ba44143757e783c1b0",
+    "url": "/static/css/main.3ff0f9c5.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ba44143757e783c1b0",
+    "url": "/static/js/main.e26d36d7.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
